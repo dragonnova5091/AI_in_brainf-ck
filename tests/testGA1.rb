b7f2142f55a5dd841f72a2385e98a3f05b300954
Ethan Hunter <Ethan Hunter>
2017-04-03 18:21:49 -0700

    first time

---
+++ tests/testGA1.rb
@@ -0,0 +1,51 @@
+require_relative "../compilier/compilier.rb"
+
+
+$bfchars = "+++---.,><[]".split("")
+
+class Chromosome
+
+	attr_accessor :chromosome
+	
+	def initialize(length)
+		@chromosome = ""
+		length.times do 
+			@chromosome += $bfchars[rand(0..$bfchars.length-1)]
+		end
+		
+	end
+	
+	def prints
+		puts @chromosome
+	end
+	
+	def new_chromosome
+		num = @chromosome.split("").length
+		@chromosome = ""
+		num.times do
+			@chromosome += $bfchars[rand(0..$bfchars.length-1)]
+			
+		end
+	end
+	
+end
+
+Brainfuck.evaluate("++++++++++[>+++++++<-]>.--.+.-----.+++++++++++.")
+
+
+testCromosome = Chromosome.new(50)
+test = false
+tests = 1
+
+while test 
+begin 
+	Brainfuck.evaluate(testCromosome.chromosome)
+	test = false
+rescue
+	testCromosome = Chromosome.new(50)
+	test = true
+	tests += 1
+end
+end
+puts testCromosome.chromosome
+
